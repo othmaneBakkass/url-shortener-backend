17282751165c55e6f4e1892df67f9529509e1d5f
othmane bakkass <othmanebakkass@gmail.com>
2024-09-23 20:07:24 +0100

    feat: fetch and delete short links

--- src/repositories/shortlinks/index.ts
+++ src/repositories/shortlinks/index.ts
@@ -34,7 +34,11 @@ export async function createShortLink({
 
 export async function getShortLinks() {
 	try {
-		const data = await db.shortLinks.findMany();
+		const data = await db.shortLinks.findMany({
+			orderBy: {
+				createdAt: "desc",
+			},
+		});
 
 		return {
 			ok: true as const,
@@ -42,6 +46,69 @@ export async function getShortLinks() {
 		};
 	} catch (error) {
 		console.log("🚀 ~ getShortLinks error ~", error);
+		return {
+			ok: false as const,
+			msg: "database error" as const,
+		};
+	}
+}
+
+export type GetShortLinksSuccessPayload = Extract<
+	Awaited<ReturnType<typeof getShortLinks>>,
+	{ ok: true }
+>;
+
+export async function getShortLinkByMiniUrl(shortLink: string) {
+	try {
+		const data = await db.shortLinks.findFirst({
+			where: {
+				short_link: shortLink,
+			},
+		});
+
+		return {
+			ok: true as const,
+			data,
+		};
+	} catch (error) {
+		console.log(
+			"🚀 ~ getShortLinkByMiniUrl error ~",
+			"shortLink:",
+			shortLink,
+			"error:",
+			error
+		);
+		return {
+			ok: false as const,
+			msg: "database error" as const,
+		};
+	}
+}
+
+export async function deleteShortLinkByID(id: string) {
+	try {
+		const data = await db.shortLinks.delete({
+			where: {
+				id,
+			},
+		});
+
+		return {
+			ok: true as const,
+			data: {
+				name: data.name,
+				short_link: data.short_link,
+				original_url: data.original_url,
+			},
+		};
+	} catch (error) {
+		console.log(
+			"🚀 ~ deleteShortLinkByID error ~",
+			"shortLink:",
+			id,
+			"error:",
+			error
+		);
 		return {
 			ok: false as const,
 			msg: "database error" as const,
